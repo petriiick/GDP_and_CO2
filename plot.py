708f7afc627542ef3aceff0e6ac1ca7d9faad996
Charlotte Yuan <yyuan@CharlotteYuan2.lan>
2022-10-03 23:22:00 -0400

    added analysis

---
+++ plot.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+wdi = pd.read_csv(
+    "https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv"
+)
+
+plot_data = wdi[
+    [
+        "Mortality rate, infant (per 1,000 live births)",
+        "GDP per capita (constant 2010 US$)",
+        "Country Name",
+    ]
+]
+
+plot_data.plot(
+    x="Mortality rate, infant (per 1,000 live births)",
+    y="GDP per capita (constant 2010 US$)",
+    kind="scatter",
+)
+plt.show()
